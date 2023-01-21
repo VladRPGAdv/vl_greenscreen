e28c491830377c771d39d64e72b220559fde5a31
VladRPG <68168122+VladRPGAdv@users.noreply.github.com>
2023-01-21 16:14:26 +0200

    Add files via upload

---
+++ vl_greenscreen/config.lua
@@ -0,0 +1,11 @@
+VL = {}
+
+VL.GreenScreenLocation = {-1038.9710693359,-3298.0625,13.944444656372}
+VL.CoordsTOTP = {-1079.5939941406,-3252.5031738281,31.424543380737}
+VL.VehicleSpawn = {-1084.8666992188,-3251.1640625,30.424444198608}
+
+VL.webhook = "https://discordapp.com/api/webhooks/1064133669813506088/4XDHbH2d_diJvpa-Kua5dAfq8kuOEG86C4qwJ2DB3kbaEfc7Hjrs2jaMZx1RYLZV3pHu"
+
+VL.VehiclesToScreenshot = {"dinghy2", "nero", "sanchez", "infernus", "sultan", "turismo2", "mule4", "phantom", "dilettante"}
+
+VL.HelpMessage = true
---
+++ vl_greenscreen/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'Pentru baieti lenesi ca mine'
+
+
+client_scripts {
+    '@vrp/client/Proxy.lua',
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    '@vrp/lib/utils.lua',
+    'config.lua',
+    'server.lua'
+}
